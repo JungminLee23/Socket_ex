542eb403b2a62ac96a97b3fe4bd7da772a72ea73
JMLee <zkrns23@gmail.com>
2020-08-03 02:31:46 +0900

    dd
    dd

--- 20200803_IPAddr_change/20200803_IPAddr_change/20200803_IPAddr_change.cpp
+++ 20200803_IPAddr_change/20200803_IPAddr_change/20200803_IPAddr_change.cpp
@@ -1,20 +1,50 @@
-﻿// 20200803_IPAddr_change.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#pragma comment(lib, "ws2_32")
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
 
-#include <iostream>
+int main() {
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) return 1;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	// IPv4 변환 연습
+
+	// 원래의 IPv4 주소 출력
+	const char * ipv4test = "147.46.114.70";
+	printf("IPv4 주소(변환 전) = %s\n", ipv4test);
+
+	//inet_addr() 함수 연습
+	printf("IPv4 주소(변환 후) = 0x%x\n", inet_addr(ipv4test));
+
+	//inet_ntoa() 함수 연습
+	IN_ADDR ipv4num;
+	ipv4num.s_addr = inet_addr(ipv4test);
+	printf("IPv4 주소(다시 변환 후) = %s\n", inet_ntoa(ipv4num));
+
+	printf("\n");
+
+	//IPv6 변환 연습
+
+	//원래의 IPv6 주소 출력
+	const char * ipv6test = "2001:0231:abcd:ffab:0023:eb00:ffff:1111";
+	printf("IPv6주소(변환 전) = %s\n", ipv6test);
+
+	//WSAStringToAddress() 함수 연습
+	SOCKADDR_IN6 ipv6num;
+	int addrlen = sizeof(ipv6num);
+	WSAStringToAddress((LPWSTR)ipv6test, AF_INET6, NULL, (SOCKADDR *)&ipv6num, &addrlen);
+	printf("IPv6 주소(변환 후) = 0x");
+	for (int i = 0; i < 16; i++) {
+		printf("%02x", ipv6num.sin6_addr.u.Byte[i]);
+	}
+	printf("\n");
+
+	//WSAAddressToString() 함수 연습
+	char ipaddr[50];
+	DWORD ipaddrlen = sizeof(ipaddr);
+	WSAAddressToString((SOCKADDR *)&ipv6num, sizeof(ipv6num), NULL, (LPWSTR)ipaddr, &ipaddrlen);
+	printf("IPv6 주소(다시 변환 후) = %s\n", ipaddr);
+
+	WSACleanup();
+	return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
